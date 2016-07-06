181a0b3fbd22dff2c60a3218a39d7ea9752cd38b
pjp <pjp@delphinusdns.org>
2016-07-06 05:16:52 +0000

    * part 2 of moving delphinusdnsd to its own subdir

---
+++ ddd-include.h
@@ -0,0 +1,87 @@
+/* 
+ * Copyright (c) 2005-2015 Peter J. Philipp
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ */
+#ifndef _INCLUDES_H
+#define _INCLUDES_H
+
+#include <sys/param.h>
+#include <sys/ipc.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <sys/queue.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+#include <net/if.h>
+
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
+#define _KERNEL 1
+#include <netinet/ip6.h>
+#undef _KERNEL
+
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <syslog.h>
+#include <ctype.h>
+#include <db.h>
+#include <pwd.h>
+#include <ifaddrs.h>
+#include <dirent.h>
+#include <signal.h>
+
+#ifdef __linux__
+#include <grp.h>
+#endif
+
+#ifndef NTOHS
+#include "endian.h"
+#endif
+
+#if !defined __OpenBSD__ && !defined __NetBSD__
+struct ip6_hdr_pseudo {
+        struct in6_addr ip6ph_src;
+        struct in6_addr ip6ph_dst;
+        u_int32_t       ip6ph_len;
+        u_int8_t        ip6ph_zero[3];
+        u_int8_t        ip6ph_nxt;
+} __packed;
+#endif
+
+#endif
