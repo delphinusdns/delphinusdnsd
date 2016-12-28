2e2f5d4505cb7af21c80cf8c2382a8ea3f9f5280
pjp <pjp@delphinusdns.org>
2016-12-28 09:06:51 +0000

    sign_cname(), which signs a cname record based on sign_ns(). This is a milestone as all records involved with my test zone are covered, but not all RR's covered by delphinusdnsd are finished for signing.  I'll need to create a new zone file to get sign comparisons with this.

--- dd-convert.c
+++ dd-convert.c
@@ -53,6 +53,7 @@ int 	sign_a(DB *, char *, char *, int, struct domain *);
 int 	sign_mx(DB *, char *, char *, int, struct domain *);
 int 	sign_ns(DB *, char *, char *, int, struct domain *);
 int 	sign_spf(DB *, char *, char *, int, struct domain *);
+int 	sign_cname(DB *, char *, char *, int, struct domain *);
 int 	sign_soa(DB *, char *, char *, int, struct domain *);
 int	sign_txt(DB *, char *, char *, int, struct domain *);
 int	sign_aaaa(DB *, char *, char *, int, struct domain *);
@@ -744,6 +745,24 @@ dump_db(DB *db)
 			}
 
 
+			if (sdomain->flags & DOMAIN_HAVE_CNAME) {
+				rss = (struct rrsig *)&sdrr->rrsig[INTERNAL_TYPE_CNAME];
+				len = mybase64_encode(rss->signature, rss->signature_len, buf, sizeof(buf));
+				buf[len] = '\0';
+
+				printf("%s,rrsig,%d,%s,%d,%d,%d,%llu,%llu,%d,%s,\"%s\"\n", 
+					sdomain->zonename,
+					sdomain->ttl[INTERNAL_TYPE_RRSIG],
+					get_dns_type(rss->type_covered, 0), 
+					rss->algorithm, rss->labels,
+					rss->original_ttl, 
+					timethuman(rss->signature_expiration),
+					timethuman(rss->signature_inception), 
+					rss->key_tag,
+					convert_name(rss->signers_name, rss->signame_len),
+					buf);	
+			}
+
 
 			if (sdomain->flags & DOMAIN_HAVE_NS) {
 				rss = (struct rrsig *)&sdrr->rrsig[INTERNAL_TYPE_NS];
@@ -1111,6 +1130,9 @@ calculate_rrsigs(DB *db, char *zonename, char *zsk_key, char *ksk_key, int expir
 				return -1;
 		if (sd->flags & DOMAIN_HAVE_SPF)
 			if (sign_spf(db, zonename, zsk_key, expiry, sd) < 0)
+				return -1;
+		if (sd->flags & DOMAIN_HAVE_CNAME)
+			if (sign_cname(db, zonename, zsk_key, expiry, sd) < 0)
 				return -1;
 
 		j++;
@@ -2314,6 +2336,237 @@ sign_spf(DB *db, char *zonename, char *zsk_key, int expiry, struct domain *sd)
 	return 0;
 }
 
+
+/*
+ * create a RRSIG for a CNAME record
+ */
+
+int
+sign_cname(DB *db, char *zonename, char *zsk_key, int expiry, struct domain *sd)
+{
+	struct domain_cname *sdc;
+
+	char tmp[4096];
+	char signature[4096];
+	char buf[512];
+	char shabuf[64];
+	
+	SHA_CTX sha1;
+	SHA256_CTX sha256;
+	SHA512_CTX sha512;
+
+	char *dnsname;
+	char *p;
+	char *key;
+	char *zone;
+
+	uint32_t ttl;
+	uint16_t flags;
+	uint8_t protocol;
+	uint8_t algorithm;
+
+	int labellen;
+	int keyid;
+	int fd, len;
+	int keylen, siglen;
+	int rsatype;
+	int bufsize;
+	int labels;
+
+	RSA *rsa;
+	time_t now;
+
+	char timebuf[32];
+	u_int64_t expiredon, signedon;
+	struct tm *tm;
+	u_int32_t expiredon2, signedon2;
+
+	memset(&shabuf, 0, sizeof(shabuf));
+
+	now = time(NULL);
+	tm = gmtime(&now);
+	strftime(timebuf, sizeof(timebuf), "%Y%m%d%H%M%S", tm);
+	signedon = atoll(timebuf);
+	now += expiry;
+	tm = gmtime(&now);
+	strftime(timebuf, sizeof(timebuf), "%Y%m%d%H%M%S", tm);
+	expiredon = atoll(timebuf);
+
+#if PROVIDED_SIGNTIME
+        signedon = SIGNEDON;
+        expiredon = EXPIREDON;
+#endif
+
+
+	key = malloc(10 * 4096);
+	if (key == NULL) {
+		dolog(LOG_INFO, "out of memory\n");
+		return -1;
+	}
+
+	/* get the ZSK */
+	snprintf(buf, sizeof(buf), "%s.key", zsk_key);
+	if ((fd = open(buf, O_RDONLY, 0)) < 0) {
+		dolog(LOG_INFO, "open %s: %s\n", buf, strerror(errno));
+		return -1;
+	}
+
+	if ((zone = parse_keyfile(fd, &ttl, &flags, &protocol, &algorithm, (char *)&tmp, &keyid)) == NULL) {
+		dolog(LOG_INFO, "parse %s\n", buf);
+		close (fd);
+		return -1;
+	}
+
+	close(fd);
+
+	/* check the keytag supplied */
+	p = key;
+	pack16(p, htons(flags));
+	p += 2;
+	pack8(p, protocol);
+	p++;
+	pack8(p, algorithm);
+	p++;
+	keylen = mybase64_decode(tmp, (char *)&signature, sizeof(signature));
+	pack(p, signature, keylen);
+	p += keylen;
+	keylen = (p - key);
+	if (keyid != keytag(key, keylen)) {
+		dolog(LOG_ERR, "keytag does not match %d vs. %d\n", keyid, keytag(key, keylen));
+		return -1;
+	}
+	
+	labels = label_count(sd->zonename);
+	if (labels < 0) {
+		dolog(LOG_INFO, "label_count");
+		return -1;
+	}
+
+	dnsname = dns_label(zonename, &labellen);
+	if (dnsname == NULL)
+		return -1;
+
+	if (sd->flags & DOMAIN_HAVE_CNAME) {
+                if ((sdc = (struct domain_cname *)find_substruct(sd, INTERNAL_TYPE_CNAME)) == NULL) {
+			dolog(LOG_INFO, "no CNAME records but have flags!\n");
+                        return -1;
+		}
+	}
+	
+	p = key;
+
+	pack16(p, htons(DNS_TYPE_CNAME));
+	p += 2;
+	pack8(p, algorithm);
+	p++;
+	pack8(p, labels);
+	p++;
+	pack32(p, htonl(sd->ttl[INTERNAL_TYPE_CNAME]));
+	p += 4;
+		
+	snprintf(timebuf, sizeof(timebuf), "%lld", expiredon);
+	strptime(timebuf, "%Y%m%d%H%M%S", tm);
+	expiredon2 = timegm(tm);
+	snprintf(timebuf, sizeof(timebuf), "%lld", signedon);
+	strptime(timebuf, "%Y%m%d%H%M%S", tm);
+	signedon2 = timegm(tm);
+
+	pack32(p, htonl(expiredon2));
+	p += 4;
+	pack32(p, htonl(signedon2));	
+	p += 4;
+	pack16(p, htons(keyid));
+	p += 2;
+	pack(p, dnsname, labellen);
+	p += labellen;
+
+	/* no signature here */	
+	/* XXX this should probably be done on a canonical sorted records */
+	
+	pack(p, sd->zone, sd->zonelen);
+	p += sd->zonelen;
+	pack16(p, htons(DNS_TYPE_CNAME));
+	p += 2;
+	pack16(p, htons(DNS_CLASS_IN));
+	p += 2;
+	pack32(p, htonl(sd->ttl[INTERNAL_TYPE_CNAME]));
+	p += 4;
+	pack16(p, htons(sdc->cnamelen));
+	p += 2;
+	pack(p, sdc->cname, sdc->cnamelen);
+	p += sdc->cnamelen;
+
+	keylen = (p - key);	
+
+#if 0
+	fd = open("bindump.bin", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	for (i = 0; i < keylen; i++) {
+		write(fd, (char *)&key[i], 1);
+	}
+	close(fd);
+	
+#endif
+
+	switch (algorithm) {
+	case ALGORITHM_RSASHA1:
+		SHA1_Init(&sha1);
+		SHA1_Update(&sha1, key, keylen);
+		SHA1_Final((u_char *)shabuf, &sha1);
+		bufsize = 20;
+		break;
+	case ALGORITHM_RSASHA256:	
+		SHA256_Init(&sha256);
+		SHA256_Update(&sha256, key, keylen);
+		SHA256_Final((u_char *)shabuf, &sha256);
+		bufsize = 32;
+
+#if 0
+		printf("keylen = %d\n", keylen);
+		fd = open("bindump-sha256.bin", O_WRONLY | O_CREAT | O_TRUNC, 0600);
+		for (i = 0; i < bufsize; i++) {
+			write(fd, (char *)&shabuf[i], 1);
+		}
+		close(fd);
+#endif
+
+		break;
+	case ALGORITHM_RSASHA512:
+		SHA512_Init(&sha512);
+		SHA512_Update(&sha512, &key, keylen);
+		SHA512_Final((u_char *)shabuf, &sha512);
+		bufsize = 64;
+		break;
+	default:
+		return -1;
+	}
+		
+	rsa = read_private_key(zonename, keyid, algorithm);
+	if (rsa == NULL) {
+		dolog(LOG_INFO, "reading private key failed\n");
+		return -1;
+	}
+		
+	rsatype = alg_to_rsa(algorithm);
+	if (rsatype == -1) {
+		dolog(LOG_INFO, "algorithm mismatch\n");
+		return -1;
+	}
+
+	if (RSA_sign(rsatype, (u_char *)shabuf, bufsize, (u_char *)signature, &siglen, rsa) != 1) {
+		dolog(LOG_INFO, "unable to sign with algorithm %d: %s\n", algorithm, ERR_error_string(ERR_get_error(), NULL));
+		return -1;
+	}
+
+	len = mybase64_encode(signature, siglen, tmp, sizeof(tmp));
+	tmp[len] = '\0';
+
+	if (fill_rrsig(sd->zonename, "RRSIG", sd->ttl[INTERNAL_TYPE_CNAME], "CNAME", algorithm, labels, sd->ttl[INTERNAL_TYPE_CNAME], expiredon, signedon, keyid, zonename, tmp) < 0) {
+		dolog(LOG_INFO, "fill_rrsig\n");
+		return -1;
+	}
+	
+	return 0;
+}
 
 
 
