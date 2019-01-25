0e88a280f7dc132ea74111dce78ed01d2fbf98ac
pjp <pjp@delphinusdns.org>
2019-01-25 20:37:31 +0000

    visual inspection through cvsweb unearthed this bug.  tsk tsk

--- delphinusdnsd.c
+++ delphinusdnsd.c
@@ -27,7 +27,7 @@
  */
 
 /*
- * $Id: delphinusdnsd.c,v 1.44 2019/01/25 20:10:24 pjp Exp $
+ * $Id: delphinusdnsd.c,v 1.45 2019/01/25 20:37:31 pjp Exp $
  */
 
 #include "ddd-include.h"
@@ -148,7 +148,7 @@ static struct reply_logic {
 	{ DNS_TYPE_MX, DNS_TYPE_NS, BUILD_OTHER, reply_ns },
 	{ DNS_TYPE_MX, DNS_TYPE_MX, BUILD_OTHER, reply_mx },
 	{ DNS_TYPE_SOA, DNS_TYPE_SOA, BUILD_OTHER, reply_soa },
-	{ DNS_TYPE_SOA, DNS_TYPE_NS, BUILD_OTHER, reply_soa },
+	{ DNS_TYPE_SOA, DNS_TYPE_NS, BUILD_OTHER, reply_ns },
 	{ DNS_TYPE_NS, DNS_TYPE_NS, BUILD_OTHER, reply_ns },
 	{ DNS_TYPE_TLSA, DNS_TYPE_TLSA, BUILD_OTHER, reply_tlsa },
 	{ DNS_TYPE_SSHFP, DNS_TYPE_SSHFP, BUILD_OTHER, reply_sshfp },
